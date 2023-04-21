8a72bffade733ac5479e41671e1240229c831331
Kaity Hayes <kaitynhayes@gmail.com>
2023-04-21 10:01:57 -0500

    setting up client side

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react'
+import './App.css';
+
+//components
+import InputBands from './components/InputBands';
+
+
+function App() {
+  return (
+  <Fragment>
+    <div className='container'>
+    <InputBands />
+    </div>
+  </Fragment>
+  )  
+}
+
+export default App;
---
+++ client/src/components/InputBands.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react'
+
+const InputBands = () => {
+    return (
+    <Fragment>
+        <h1 className='text-ceneter mt-5'>Book bands</h1>
+        <form>
+            <input text='text' />
+            <button>Add</button>
+        </form>
+    </Fragment>
+    ) 
+}
+
+export default InputBands;
